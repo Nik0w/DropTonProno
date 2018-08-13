03c057efd614cb6afadad2fd08ff0e7b07070607
Nikow <nicolas.larrode@gmail.com>
2018-08-13 14:37:11 +0200

    style + début dev classement

--- app/Http/Controllers/ClassementController.php
+++ app/Http/Controllers/ClassementController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+use Auth;
 
 class ClassementController extends Controller
 {
@@ -23,6 +27,14 @@ class ClassementController extends Controller
      */
     public function index()
     {
-        return view('classement');
+        $users = DB::table('users')->get();
+        $user = DB::table('users')
+                ->where('users.id','=',Auth::id())
+                ->first();
+
+        return view('classement',[
+            'users' => $users,
+            'user' => $user
+        ]);
     }
 }
--- resources/views/classement.blade.php
+++ resources/views/classement.blade.php
@@ -2,32 +2,51 @@
 
 @section('content')
 
-@include('includes.menu')
 <div class="bg-img">
 
+    @include('includes.menu')
+
+    @include('includes.reglement')
+
     <div class="nuages">
-            <img class="nuage" src="img/nuages1.png" alt="">
-            <img class="nuage" src="img/nuages2.png" alt="">
-            <img class="nuage" src="img/nuages3.png" alt="">
+            <img class="nuage" src="{{asset('/img/nuages1.png')}}" alt="">
+            <img class="nuage" src="{{asset('/img/nuages2.png')}}" alt="">
+            <img class="nuage" src="{{asset('/img/nuages3.png')}}" alt="">
     </div>
 
 
     <div class="container">
 
-        <div class="row resultats">
+        
+        <div class="row classement">
             <div class=".d-none .d-sm-block col-md-8 encartPub"></div>
 
-            <div class="col-12 text-center">
-                <h1>Classement des joueurs</h1>
-                <p>Bienvenue à toi l'ami</p>
-                <p>Viens faire tes pronos ! Victoire de l'UBB ? Défaite de Clermont ?</p>
-                <p>Monte au classement pour gagner des >cadeaux</p>
+            <div class="col-12 text-resultats color-blue text-center">
+              <h1>Classement des joueurs</h1>
+              <p>Bienvenue à toi l'ami<br />
+              Viens faire tes pronos ! Victoire de l'UBB ? Défaite de Clermont ?<br />
+              Monte au classement pour <button class="small-cta bg-orange">gagner des cadeaux</button></p>
             </div>
 
-        </div>
+            <div class="col-12 col-md-8 offset-md-2 bg-orange classement-user">
+              <div class="row">              
+                <div class="col-5">
+                  <div class="nameUser">{{$user->name}}</div>
+                </div>
+                <div class="col-offset-1"></div>
+                <div class="col-2 text-center">
+                  <div>Bons pronos</div>
+                </div>
+                <div class="col-2 text-center">
+                  <div>Score exacts</div>
+                </div>
+                <div class="col-2 text-center">
+                  <div>Points</div>
+                </div>
+              </div>
+            </div>
 
-        <div class="row classement">
-            <table class="table table-striped bg-white">
+            <table class="table table-striped bg-white col-md-8 offset-md-2">
               <thead class="bg-bleu lato">
                 <tr>
                   <th scope="col">Rang</th>
@@ -38,47 +57,42 @@
                 </tr>
               </thead>
               <tbody>
-                <tr class="top1">
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>32</td>
-                  <td>32</td>
-                  <td>32</td>
-                </tr>
-                <tr class="top2">
-                  <th scope="row">2</th>
-                  <td>Mark</td>
-                  <td>32</td>
-                  <td>32</td>
-                  <td>32</td>
-                </tr>
-                <tr class="top3">
-                  <th scope="row">3</th>
-                  <td>Mark</td>
-                  <td>32</td>
-                  <td>32</td>
-                  <td>32</td>
-                </tr>
-                <tr>
-                  <th scope="row">4</th>
-                  <td>Mark</td>
-                  <td>32</td>
-                  <td>32</td>
-                  <td>32</td>
-                </tr>
-                <tr>
-                  <th scope="row">5</th>
-                  <td>Mark</td>
-                  <td>32</td>
-                  <td>32</td>
-                  <td>32</td>
-                </tr>
+                @foreach($users as $u)
+                  <tr class="top1">
+                    <th scope="row">1</th>
+                    <td>{{$u->name}}</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr class="top2">
+                    <th scope="row">2</th>
+                    <td>Mark</td>
+                    <td>32</td>
+                    <td>32</td>
+                    <td>32</td>
+                  </tr>
+                  <tr class="top3">
+                    <th scope="row">3</th>
+                    <td>Mark</td>
+                    <td>32</td>
+                    <td>32</td>
+                    <td>32</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">4</th>
+                    <td>Mark</td>
+                    <td>32</td>
+                    <td>32</td>
+                    <td>32</td>
+                  </tr>
+                @endforeach
               </tbody>
             </table>
+
         </div>
 
-        
     </div>
-    
+
 </div>
 @endsection
