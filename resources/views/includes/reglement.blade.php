e23ae5e4b4eefa101e9ce736a0515e4950912af5
Nikow <nicolas.larrode@gmail.com>
2018-08-20 02:36:49 +0200

    modifs pour mise en ligne

--- resources/views/includes/reglement.blade.php
+++ resources/views/includes/reglement.blade.php
@@ -3,21 +3,27 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Réglement :</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Règlement :</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body lato">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis orci in eros volutpat tristique. Etiam eleifend nunc vel ipsum malesuada pulvinar. Maecenas purus dolor, tempus eu suscipit mollis, vulputate eget leo. Cras porta ante ante, in posuere libero posuere euismod. Nunc ullamcorper massa a consequat faucibus. Sed sit amet est sodales, viverra elit ultricies, imperdiet neque. Quisque posuere metus vel purus consequat volutpat. Aliquam laoreet massa aliquet dui pharetra interdum. Maecenas maximus tincidunt turpis sed venenatis. Phasellus eget turpis ullamcorper, pellentesque turpis a, suscipit nibh. Curabitur sit amet imperdiet magna. Suspendisse cursus tincidunt vestibulum. Vestibulum placerat rhoncus fringilla. Integer eu porttitor diam, quis lobortis nisi. Morbi iaculis in elit at euismod.<br /><br />
 
-Phasellus ornare libero ac egestas malesuada. Vivamus ut laoreet massa. Nulla erat risus, vestibulum eu ultricies vitae, scelerisque ut mi. Etiam facilisis, justo eu faucibus sagittis, quam orci scelerisque magna, non suscipit elit nunc et quam. Phasellus maximus felis non ullamcorper imperdiet. Pellentesque erat turpis, venenatis eu ex id, blandit scelerisque ante. Nulla mattis risus ut massa placerat dictum. Integer fermentum dolor erat, et lacinia felis varius vitae. Curabitur quis sapien dapibus, blandit ex convallis, lacinia libero. Etiam tincidunt nisl sit amet tortor blandit pulvinar. Aliquam fermentum lacus at ante dapibus vehicula.<br /><br />
+Pronostiquez le score exact et le nombre d’essais lors de chaque journée du Top 14 et grimpez au classement pour décrocher le Bouclier de Brennus ! 
+Le premier au classement de chaque mois se verra remettre un cadeau ainsi que les trois meilleurs sur l’ensemble de la saison. <br /><br />
 
-Vivamus sed odio eget diam tristique pulvinar. Morbi maximus risus eget mi tempus suscipit. Pellentesque viverra justo nec faucibus aliquet. Fusce gravida, nunc eu pulvinar aliquam, sem neque aliquet quam, in viverra neque ligula et tellus. Ut in velit elementum, tempor quam in, consequat arcu. Pellentesque nulla sapien, pharetra ac sem ac, accumsan pellentesque turpis. Pellentesque vel orci sed dolor sodales fermentum. Vivamus purus urna, volutpat ut mollis ac, consectetur et dolor. Nam egestas eget est ut porttitor.<br /><br />
+Si je pronostique,<br /><br />
 
-Aliquam fringilla placerat nisl, sit amet condimentum neque laoreet vel. Donec varius maximus orci in semper. Vestibulum volutpat massa ac dolor accumsan, in pulvinar elit viverra. In vel ex ut nisl luctus tincidunt vitae sit amet eros. Etiam consequat urna id tortor mattis, sed pulvinar arcu eleifend. Donec eget massa eget lectus tincidunt accumsan non id felis. Nullam quis magna sapien. Curabitur iaculis nulla ut erat dictum, a mollis libero rhoncus. Sed eu tempus elit.<br /><br />
+La victoire, 5 points
+Score +/- de 3 points d'un équipe, 3 points
+Score +/- de 5 points des deux équipes, 6 points
+Le nul, 30 points
+Le score excat d’une équipe, 20 points
+Le score excat des deux équipes, 40 points 
+Le nombre d’essais inscrits dans le match, 10 points.<br /><br />
 
-Aliquam porta, mauris quis aliquam ultricies, leo ex tempor arcu, pulvinar maximus ante nisl ac odio. Etiam semper elementum dui, sed pharetra metus efficitur a. Integer eget massa lacus. Nam diam ante, congue eu sem eu, cursus sagittis justo. Nulla porta pellentesque sem. Nunc vestibulum pretium luctus. Mauris accumsan ligula ac ante varius, id facilisis massa elementum. Pellentesque lacinia quam libero, ac placerat arcu ultrices in. Praesent aliquam, erat nec venenatis hendrerit, orci risus ultrices nibh, quis placerat libero nulla et nisi. Donec convallis pulvinar porttitor.
+Bonne chance à tous ! <br /><br />
       </div>
       <div class="modal-footer">
         &copy; Drop Ton Prono 2018
